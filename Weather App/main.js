36dce81bd325e6d8a2651fb447770a0c1fa30d0b
John Escamilla <jbescamilla6@gmail.com>
2022-03-26 18:42:34 -0700

    Not your mothers weather app

---
+++ Weather App/main.js
@@ -0,0 +1,10 @@
+//assign values to ids
+let searchCity = document.querySelector('#city')
+let cityText = document.querySelector('#city_text')
+let temp = document.querySelector('#temperature')
+let ferHeight = document.querySelector('#fHeight')
+let celcius = document.querySelector('#celcius')
+
+//create function to fetch api on search and show temp-city
+
+//on click convert fheight to celcius
